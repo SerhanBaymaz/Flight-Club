1db5586d9cb5f8b803f19970acbf25529bc5f8f3
İbrahim Baymaz <20200808006@ogr.akdeniz.edu.tr>
2023-05-16 16:46:41 +0300

    Binding activated.

--- app/src/main/java/com/example/book_flight/MainActivity.kt
+++ app/src/main/java/com/example/book_flight/MainActivity.kt
@@ -2,12 +2,16 @@ package com.example.book_flight
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.book_flight.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding= ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        println("Testing the repo last time")
+
     }
 }
