c846a62375905b60717440ba44e19e06cd22a610
İbrahim Baymaz <20200808006@ogr.akdeniz.edu.tr>
2023-05-30 20:34:46 +0300

    Bottom navigation bar added.

--- app/src/main/java/com/example/book_flight/Home_activity.kt
+++ app/src/main/java/com/example/book_flight/Home_activity.kt
@@ -1,13 +1,13 @@
 package com.example.book_flight
 
-import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.ArrayAdapter
+import androidx.fragment.app.Fragment
 import com.example.book_flight.databinding.ActivityHomeBinding
-import java.text.SimpleDateFormat
-import java.util.Calendar
-import java.util.Locale
+import com.example.book_flight.fragments.HomeFragment
+import com.example.book_flight.fragments.ProfileFragment
+import com.example.book_flight.fragments.TicketFragment
+
 
 class Home_activity : AppCompatActivity() {
 
@@ -18,42 +18,31 @@ class Home_activity : AppCompatActivity() {
         binding = ActivityHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        val homeFragment = HomeFragment()
+        val ticketFragment = TicketFragment()
+        val profileFragment = ProfileFragment()
 
-        //val abc = arrayOf("a","b","c")
-        val seatclass = resources.getStringArray(R.array.seatclass)
-        val number_of_passengers = resources.getStringArray(R.array.passengers)
+        makeCurrentFragment(homeFragment)
 
-        val arrayAdapter_seat = ArrayAdapter(this, R.layout.list_item, R.id.textview_seatclasslist,seatclass)
-        binding.autoCompleteTextViewSeatclass.setAdapter(arrayAdapter_seat)
+        binding.bottomNavigation.setOnNavigationItemSelectedListener {
+            when(it.itemId){
+                R.id.ic_home ->makeCurrentFragment(homeFragment)
+                R.id.ic_profile ->makeCurrentFragment(profileFragment)
+                R.id.ic_ticket ->makeCurrentFragment(ticketFragment)
 
-        val arrayAdapter_passengers = ArrayAdapter(this, R.layout.list_item, R.id.textview_passengerlist,number_of_passengers)
-        binding.autoCompleteTextViewPassengers.setAdapter(arrayAdapter_passengers)
-
-
-        val myCalendar = Calendar.getInstance()
-
-        val datePicker = DatePickerDialog.OnDateSetListener{ view, year, month, dayOfMonth ->
-            myCalendar.set(Calendar.YEAR,year)
-            myCalendar.set(Calendar.MONTH,month)
-            myCalendar.set(Calendar.DAY_OF_MONTH,dayOfMonth)
-            updateLable(myCalendar)
+            }
+            true
         }
-
-        binding.btnDatePicker.setOnClickListener {
-            DatePickerDialog(this,datePicker,myCalendar.get(Calendar.YEAR),
-                myCalendar.get(Calendar.MONTH),myCalendar.get(Calendar.DAY_OF_MONTH)).show()
-        }
-
-
 
     }//oncreate
 
-    fun updateLable(myCalendar: Calendar){
-        val myFormat = "dd-MM-yyyy"
-        val simpleDateFormat = SimpleDateFormat(myFormat, Locale.ENGLISH)
 
-        binding.tvDatePicker.setText(simpleDateFormat.format(myCalendar.time))
-
+    fun makeCurrentFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.fl_wrapper,fragment)
+            commit()
+        }
     }
+
 
 }//class
---
+++ app/src/main/java/com/example/book_flight/fragments/HomeFragment.kt
@@ -0,0 +1,120 @@
+package com.example.book_flight.fragments
+
+import android.app.DatePickerDialog
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.TextView
+import com.example.book_flight.R
+import com.example.book_flight.databinding.ActivityHomeBinding
+import com.example.book_flight.databinding.FragmentHomeBinding
+import java.text.SimpleDateFormat
+import java.util.Calendar
+import java.util.Locale
+
+
+class HomeFragment : Fragment(R.layout.fragment_home) {
+
+    lateinit var binding : FragmentHomeBinding
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        binding = FragmentHomeBinding.bind(view)
+
+
+        //val abc = arrayOf("a","b","c")
+        val seatclass = resources.getStringArray(R.array.seatclass)
+        val number_of_passengers = resources.getStringArray(R.array.passengers)
+
+        val arrayAdapter_seat =
+            activity?.let { ArrayAdapter(it, R.layout.list_item, R.id.textview_seatclasslist, seatclass) }
+        binding.autoCompleteTextViewSeatclass.setAdapter(arrayAdapter_seat)
+
+        val arrayAdapter_passengers = activity?.let {
+            ArrayAdapter(
+                it,
+                R.layout.list_item,
+                R.id.textview_passengerlist,
+                number_of_passengers
+            )
+        }
+        binding.autoCompleteTextViewPassengers.setAdapter(arrayAdapter_passengers)
+
+
+        val myCalendar = Calendar.getInstance()
+
+        val datePicker = DatePickerDialog.OnDateSetListener { view, year, month, dayOfMonth ->
+            myCalendar.set(Calendar.YEAR, year)
+            myCalendar.set(Calendar.MONTH, month)
+            myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth)
+            updateLable(myCalendar)
+        }
+
+        binding.btnDatePicker.setOnClickListener {
+            activity?.let { it1 ->
+                DatePickerDialog(
+                    it1, datePicker, myCalendar.get(Calendar.YEAR),
+                    myCalendar.get(Calendar.MONTH), myCalendar.get(Calendar.DAY_OF_MONTH)
+                ).show()
+            }
+        }
+
+
+    }
+
+
+    /*
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+
+        //val abc = arrayOf("a","b","c")
+        val seatclass = resources.getStringArray(R.array.seatclass)
+        val number_of_passengers = resources.getStringArray(R.array.passengers)
+
+        val arrayAdapter_seat =
+            ArrayAdapter(activity, R.layout.list_item, R.id.textview_seatclasslist, seatclass)
+        binding.autoCompleteTextViewSeatclass.setAdapter(arrayAdapter_seat)
+
+        val arrayAdapter_passengers = ArrayAdapter(
+            this,
+            R.layout.list_item,
+            R.id.textview_passengerlist,
+            number_of_passengers
+        )
+        binding.autoCompleteTextViewPassengers.setAdapter(arrayAdapter_passengers)
+
+
+        val myCalendar = Calendar.getInstance()
+
+        val datePicker = DatePickerDialog.OnDateSetListener { view, year, month, dayOfMonth ->
+            myCalendar.set(Calendar.YEAR, year)
+            myCalendar.set(Calendar.MONTH, month)
+            myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth)
+            updateLable(myCalendar)
+        }
+
+        binding.btnDatePicker.setOnClickListener {
+            DatePickerDialog(
+                this, datePicker, myCalendar.get(Calendar.YEAR),
+                myCalendar.get(Calendar.MONTH), myCalendar.get(Calendar.DAY_OF_MONTH)
+            ).show()
+        }
+
+
+    }//oncreate
+
+*/
+
+    fun updateLable(myCalendar: Calendar) {
+        val myFormat = "dd-MM-yyyy"
+        val simpleDateFormat = SimpleDateFormat(myFormat, Locale.ENGLISH)
+
+        binding.tvDatePicker.setText(simpleDateFormat.format(myCalendar.time))
+
+    }
+
+
+}
